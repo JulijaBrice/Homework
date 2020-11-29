5b42a7ea19d45db407062428fb7c8576e6866c9d
JulijaBrice <71398398+JulijaBrice@users.noreply.github.com>
2020-11-29 18:50:33 +0200

    drag and drop added
    updated

--- final_work/views/listView.php
+++ final_work/views/listView.php
@@ -23,7 +23,18 @@
           <a href="/final_work/?page=list&action=modify">Add task</a>
           </h3>
           <div>
-            <ul class="todo-list">
+
+          <!-- Drag drom from  https://jqueryui.com/sortable/  -->
+
+            <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+            <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+            <script>
+              $( function() {
+                $( "#sortable" ).sortable({items: '.todo'});
+                $( "ul, li, a, input" ).disableSelection();
+              } );
+            </script>
+            <ul class="todo-list"  id="sortable" >
               <!--Show only not done -->
               <?php foreach($this->taskList as $task) {
                 if($task["state"] != 0){
@@ -32,16 +43,17 @@
                 ?>
                 <div class="todo" >
                   <input class="checkbox" task_id="<?=$task["id"]?>" type="checkbox" <?= $task["state"]!="0" ? "checked" : "" ?>>
-                  <i class="fas fa-check" aria-hidden="true"></i>
                   <?php
                     $form = new modifyForm($task["description"], $task["state"], $task["id"]);
                     $form->html_hidden();
                   ?>
-                  <li class="todo-item <?= $task["state"]!="0" ? "strike" : "" ?>" >        <input type="text" value="<?= $task["description"]?>" class="input_long"></li>
+                  <input readonly type="text" value="<?= $task["description"]?>" class="input_long">
                   <a class="edit_button" href="/final_work/?page=list&action=modify&task_id=<?= $task['id']?>">Edit</a>
                   <a class="trash-btn" href="/final_work/?page=delete&task_id=<?= $task['id']?>">Delete</a>
                 </div>
               <?php } ?>
+            </ul>
+            <ul class="todo-list">
               <!-- Show only Done -->
               <?php foreach($this->taskList as $task) { 
                 if($task["state"] != 1){
@@ -49,12 +61,12 @@
                 }
                 ?>
                 <div class="todo completed">
-                <input class="checkbox" task_id="<?=$task["id"]?>" type="checkbox" <?= $task["state"]!="0" ? "checked" : "" ?>>
+                <input class="checkbox-done" task_id="<?=$task["id"]?>" type="checkbox" <?= $task["state"]!="0" ? "checked" : "" ?>>
                 <?php
                   $form = new modifyForm($task["description"], $task["state"], $task["id"]);
                   $form->html_hidden();
                   ?>
-                 <li class="strike" ><input type="text" value="<?= $task["description"]?>" class="input_long"></li>
+                 <input type="text" value="<?= $task["description"]?>" class="input_long strike">
                  <a class="trash-btn" href="/final_work/?page=delete&task_id=<?= $task['id']?>">Delete</a>
                 </div>
               <?php } ?>
